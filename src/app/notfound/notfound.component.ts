bdc28e19f625b47f52d8bd6e04293e5f28d0f52a
Dev01-logic <127704528+Dev01-logic@users.noreply.github.com>
2023-03-19 17:32:18 +0700

    2 files modified

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import liff from '@line/liff';
+import { HttpClient } from '@angular/common/http';
+
+type UnPromise<T> = T extends Promise<infer X> ? X : T;
 
 @Component({
   selector: 'app-notfound',
@@ -7,11 +11,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./notfound.component.css'],
 })
 export class NotfoundComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
+  os: ReturnType<typeof liff.getOS>;
+  profile: UnPromise<ReturnType<typeof liff.getProfile>>;
 
-  ngOnInit() {}
+  ngOnInit() {
+    liff.init({ liffId: '1660756547-zRWjKKmP' }).then(() => {
+      this.os = liff.getOS();
+      if (liff.isLoggedIn()) {
+        liff
+          .getProfile()
+          .then((profile) => {
+            this.profile = profile;
+            //console.log(this.profile.userId);
+          })
+          .catch(console.error);
+      } else {
+        liff.login();
+      }
+    });
+  }
 
   onTest2(event?: MouseEvent) {
+    this.router.navigate(['home']);
+  }
+
+  onlogout(event?: MouseEvent) {
+    liff.logout();
     this.router.navigate(['home']);
   }
 }
