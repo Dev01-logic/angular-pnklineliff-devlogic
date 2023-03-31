fed0c42305fd5b08b855dce46a7b51b2f8773b77
Dev01-logic <127704528+Dev01-logic@users.noreply.github.com>
2023-03-31 10:51:52 +0700

    1 file modified

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -43,7 +43,7 @@ export class PatientComponent implements OnInit {
           })
           .catch(console.error);
       } else {
-        //liff.login();
+        liff.login();
       }
     });
 
